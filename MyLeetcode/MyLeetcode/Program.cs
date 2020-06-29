8e99e848e816fbc5e1a6e12db53253a537b23b34
CatImmortal <39085172+CatImmortal@users.noreply.github.com>
2020-06-29 23:57:29 +0800

    提交树相关题目

--- MyLeetcode/MyLeetcode/Program.cs
+++ MyLeetcode/MyLeetcode/Program.cs
@@ -63,33 +63,13 @@ namespace MyLeetcode
             public TreeNode(int x) { val = x; }
         }
 
-
-        public TreeNode TrimBST(TreeNode root, int L, int R)
+        public IList<string> BinaryTreePaths(TreeNode root)
         {
-            if (root == null)
-            {
-                return null;
-            }
 
-            if (root.val > R)
-            {
-                //根节点值比右边界大 返回修剪后的左子树
-                return TrimBST(root.left, L, R);
-            }
-
-            if (root.val < L)
-            {
-                //根节点值比左边界大 返回修剪后的右子树
-                return TrimBST(root.right, L, R);
-            }
-
-            //否则两边都修剪
-            root.left = TrimBST(root.left, L, R);
-            root.right = TrimBST(root.right, L, R);
-            return root;
         }
 
-      
+
+
     }
 
 
---
+++ MyLeetcode/MyLeetcode/树/_0257二叉树的所有路径.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeetcode.树
+{
+    //https://leetcode-cn.com/problems/binary-tree-paths/
+
+    class _0257二叉树的所有路径
+    {
+    }
+}
