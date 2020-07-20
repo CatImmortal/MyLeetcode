1501cb121616dc0e65be7bbc16d55e7cde5a9541
CatImmortal <39085172+CatImmortal@users.noreply.github.com>
2020-07-21 00:00:37 +0800

    提交数组，树相关题目

---
+++ MyLeetcode/MyLeetcode/数组/_0167两数之和2-输入有序数组.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeetcode.数组
+{
+    //https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+
+    class _0167两数之和2_输入有序数组
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int i = 0;
+            int j = numbers.Length - 1;
+            int[] result = new int[2];
+            while (i < j)
+            {
+                int sum = numbers[i] + numbers[j];
+
+                if (sum == target)
+                {
+                    result[0] = i + 1;
+                    result[1] = j + 1;
+                    break;
+                }
+
+                if (sum < target)
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MyLeetcode/MyLeetcode/树/_0429N叉树的层序遍历.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeetcode.树
+{
+    //https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/
+
+    class _0429N叉树的层序遍历
+    {
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+            Queue<Node> queue1 = new Queue<Node>();
+            Queue<Node> queue2 = new Queue<Node>();
+
+            queue1.Enqueue(root);
+
+            while (queue1.Count > 0)
+            {
+                List<int> list = new List<int>();
+                result.Add(list);
+
+                //把queue1里的所有node都出队，并把它们的子节点都暂时放进queue2里
+                while (queue1.Count > 0)
+                {
+                    Node node = queue1.Dequeue();
+                    list.Add(node.val);
+
+                    foreach (Node child in node.children)
+                    {
+                        queue2.Enqueue(child);
+                    }
+                }
+
+                //把queue2里的所有node都放进queue1中
+                while (queue2.Count > 0)
+                {
+                    queue1.Enqueue(queue2.Dequeue());
+                }
+            }
+
+            return result;
+        }
+    }
+}
