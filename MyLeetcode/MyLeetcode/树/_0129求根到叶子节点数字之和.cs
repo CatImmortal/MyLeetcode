c10329861f998c8598012e7955b4c83824d9afda
CatImmortal <1304085906@qq.com>
2020-07-21 19:22:49 +0800

    提交树相关题目

---
+++ MyLeetcode/MyLeetcode/树/_0129求根到叶子节点数字之和.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeetcode.树
+{
+    //https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/
+
+    class _0129求根到叶子节点数字之和
+    {
+
+        public int SumNumbers(TreeNode root)
+        {
+
+        }
+    }
+}
--- MyLeetcode/MyLeetcode/树/_0508出现次数最多的子树元素和.cs
+++ MyLeetcode/MyLeetcode/树/_0508出现次数最多的子树元素和.cs
@@ -4,67 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyLeetcode
+namespace MyLeetcode.树
 {
-    //Definition for singly-linked list.
-    public class ListNode
+    //https://leetcode-cn.com/problems/most-frequent-subtree-sum/
+
+    class _0508出现次数最多的子树元素和
     {
-        public int val;
-        public ListNode next;
-        public ListNode(int x) { val = x; }
-    }
-
-    // Definition for a Node.
-    public class Node
-    {
-        public int val;
-        public IList<Node> children;
-
-        public Node() { }
-
-        public Node(int _val)
-        {
-            val = _val;
-        }
-
-        public Node(int _val, IList<Node> _children)
-        {
-            val = _val;
-            children = _children;
-        }
-    }
-
-    //Definition for a binary tree node.
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int x) { val = x; }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Program p = new Program();
-
-            TreeNode n1 = new TreeNode(3);
-            TreeNode n2 = new TreeNode(1);
-            TreeNode n3 = new TreeNode(4);
-
-            TreeNode n4 = new TreeNode(2);
-            //TreeNode n5 = new TreeNode(0);
-            //TreeNode n6 = new TreeNode(3);
-
-            n1.left = n2;
-            n1.right = n3;
-
-            n2.right = n4;
-
-
-        }
-
         //key-子树元素和 value-出现次数
         private Dictionary<int, int> dict = new Dictionary<int, int>();
 
@@ -116,17 +61,5 @@ namespace MyLeetcode
 
             return root.val;
         }
-
-
     }
-
-
 }
-
-
-
-
-
-
-
-
