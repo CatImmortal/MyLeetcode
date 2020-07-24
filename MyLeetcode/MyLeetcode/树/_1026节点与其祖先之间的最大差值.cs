188ed83d406a9a72a07671deecb80d1980a70f77
CatImmortal <1304085906@qq.com>
2020-07-24 17:57:07 +0800

    提交树相关题目

--- MyLeetcode/MyLeetcode/树/_1026节点与其祖先之间的最大差值.cs
+++ MyLeetcode/MyLeetcode/树/_1026节点与其祖先之间的最大差值.cs
@@ -10,11 +10,40 @@ namespace MyLeetcode.树
 
     class _1026节点与其祖先之间的最大差值
     {
+        private int ans = 0;
+
         public int MaxAncestorDiff(TreeNode root)
         {
+            //在遍历每条路径时保存当前路径的最小值和最大值
+            //到达叶子节点后更新最小值最大值的最大差值ans
+            PreOrder(root, root.val, root.val);
 
+            return ans;
         }
 
+        private void PreOrder(TreeNode root,int min,int max)
+        {
+            if (root == null)
+            {
+                return;
+            }
 
+            //更新此条路径的最小值和最大值
+            min = Math.Min(min, root.val);
+            max = Math.Max(max, root.val);
+
+
+            //到达路径尽头 更新ans
+            if (root.left == null && root.right == null)
+            {
+                int num = Math.Abs(max - min);
+                ans = Math.Max(ans, num);
+            }
+
+            PreOrder(root.left, min, max);
+            PreOrder(root.right, min, max);
+        }
+
+   
     }
 }
