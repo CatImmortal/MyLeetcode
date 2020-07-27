633c06fe2ab1d8b605ec22f2d16205497e849d1e
CatImmortal <39085172+CatImmortal@users.noreply.github.com>
2020-07-27 23:54:26 +0800

    提交树相关题目

--- MyLeetcode/MyLeetcode/树/_0113路径总和2.cs
+++ MyLeetcode/MyLeetcode/树/_0113路径总和2.cs
@@ -10,5 +10,43 @@ namespace MyLeetcode.树
 
     class _0113路径总和2
     {
+        private IList<IList<int>> ans = new List<IList<int>>();
+
+        public IList<IList<int>> PathSum(TreeNode root, int sum)
+        {
+            PreOrder(root, new List<int>(), sum);
+            return ans;
+        }
+
+        private void PreOrder(TreeNode root,List<int> list,int sum)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            list.Add(root.val);
+
+            //处理叶子节点的情况
+            if (root.left == null && root.right == null && root.val == sum)
+            {
+
+                //加入解答集合
+
+                List<int> ansList = new List<int>(list.Count);
+                for (int i = 0; i < list.Count; i++)
+                {
+                    ansList.Add(list[i]);
+                }
+
+                ans.Add(ansList);
+            }
+
+            PreOrder(root.left, list,sum - root.val);
+            PreOrder(root.right, list,sum - root.val);
+
+            //退出前把自身移除掉
+            list.RemoveAt(list.Count - 1);
+        }
     }
 }
