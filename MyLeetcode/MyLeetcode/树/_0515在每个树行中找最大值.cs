2e7ba303f6c0d5c3f541e6294e7e5de6c81b9165
CatImmortal <1304085906@qq.com>
2020-07-27 19:23:11 +0800

    提交树相关题目

---
+++ MyLeetcode/MyLeetcode/树/_0515在每个树行中找最大值.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeetcode.树
+{
+    //https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row/
+
+    class _0515在每个树行中找最大值
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            if (root == null)
+            {
+                return result;
+            }
+            Queue<TreeNode> queue1 = new Queue<TreeNode>();
+            Queue<TreeNode> queue2 = new Queue<TreeNode>();
+
+            queue1.Enqueue(root);
+
+            while (queue1.Count > 0)
+            {
+                //记录当前层最大值
+                int max = int.MinValue;
+
+                //把queue1里的所有node都出队，并把它们的子节点都暂时放进queue2里
+                while (queue1.Count > 0)
+                {
+                    TreeNode node = queue1.Dequeue();
+
+                    if (node.val > max)
+                    {
+                        max = node.val;
+                    }
+
+                    if (node.left != null)
+                    {
+                        queue2.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue2.Enqueue(node.right);
+                    }
+                }
+
+                result.Add(max);
+
+
+                //把queue2里的所有node都放进queue1中
+                while (queue2.Count > 0)
+                {
+                    queue1.Enqueue(queue2.Dequeue());
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ MyLeetcode/MyLeetcode/树/_0919完全二叉树插入器.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeetcode.树
+{
+    //https://leetcode-cn.com/problems/complete-binary-tree-inserter/
+
+    class _0919完全二叉树插入器
+    {
+        public class CBTInserter
+        {
+
+            private List<TreeNode> nodes = new List<TreeNode>();
+
+            public CBTInserter(TreeNode root)
+            {
+                nodes.Add(new TreeNode(-1));
+
+                //按层序遍历将整棵树收集到nodes里
+                LevelOrder(root);
+            }
+
+            private void LevelOrder(TreeNode root)
+            {
+                Queue<TreeNode> queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+                while (queue.Count > 0)
+                {
+                    TreeNode node = queue.Dequeue();
+
+                    nodes.Add(node);
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+            }
+
+            public int Insert(int v)
+            {
+                TreeNode node = new TreeNode(v);
+                nodes.Add(node);
+
+                int parentPos = (nodes.Count - 1) / 2;
+                TreeNode parent = nodes[parentPos];
+
+
+                if ((nodes.Count - 1) % 2 == 0)
+                {
+                    parent.left = node;
+                }
+                else
+                {
+                    parent.right = node;
+                }
+
+                return parent.val;
+            }
+
+            public TreeNode Get_root()
+            {
+                return nodes[1];
+            }
+        }
+    }
+}
