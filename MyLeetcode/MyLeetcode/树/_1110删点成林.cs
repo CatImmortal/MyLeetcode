4e19a8b7efe4f7d9f62883f423e0ca8b332c1ccc
CatImmortal <39085172+CatImmortal@users.noreply.github.com>
2020-07-31 21:22:08 +0800

    提交树相关题目

--- MyLeetcode/MyLeetcode/树/_1110删点成林.cs
+++ MyLeetcode/MyLeetcode/树/_1110删点成林.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +11,35 @@ namespace MyLeetcode.树
 
     class _1110删点成林
     {
-        private HashSet<int> set;
+        private HashSet<int> deleteSet;
+        
         private List<TreeNode> ans = new List<TreeNode>();
 
         public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete)
         {
-            set = new HashSet<int>(to_delete);
+            if (root == null )
+            {
+                return ans;
+            }
+
+            if ( to_delete == null ||to_delete.Length == 0)
+            {
+                ans.Add(root);
+                return ans;
+            }
+
+            deleteSet = new HashSet<int>(to_delete);
+
+            if (!deleteSet.Contains(root.val))
+            {
+                ans.Add(root);
+            }
+
             TreeNode dummyRoot = new TreeNode(-1);
             dummyRoot.right = root;
             PreOrder(dummyRoot);
+
+            
 
             return ans;
            
@@ -32,19 +53,80 @@ namespace MyLeetcode.树
             }
 
 
+            TreeNode left = root.left;
+            TreeNode right = root.right;
 
-            if (root.left != null && set.Contains(root.left.val))
+            if (root.left != null && deleteSet.Contains(root.left.val))
             {
                 //左子节点需要删除
+                root.left = null;
+
+                //尝试将左子节点的左右子节点加入ans中
+                if (left.left!=null && !deleteSet.Contains(left.left.val))
+                {
+                    ans.Add(left.left);
+                }
+                if (left.right != null && !deleteSet.Contains(left.right.val))
+                {
+                    ans.Add(left.right);
+                }
             }
 
-            if (root.right != null && set.Contains(root.right.val))
+            if (root.right != null && deleteSet.Contains(root.right.val))
             {
                 //右子节点需要删除
+                root.right = null;
+
+                //尝试将右子节点的左右子节点加入ans中
+                if (right.left != null && !deleteSet.Contains(right.left.val))
+                {
+                    ans.Add(right.left);
+                }
+                if (right.right != null && !deleteSet.Contains(right.right.val))
+                {
+                    ans.Add(right.right);
+                }
             }
 
-            PreOrder(root.left);
-            PreOrder(root.right);
+            PreOrder(left);
+            PreOrder(right);
+        }
+
+        public IList<TreeNode> DelNodes2(TreeNode root, int[] to_delete)
+        {
+            deleteSet = new HashSet<int>(to_delete);
+            PostOrder(root, false);
+            return ans;
+        }
+
+        private bool PostOrder(TreeNode root,bool parentExist)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            bool needDelete = deleteSet.Contains(root.val);
+
+            if (PostOrder(root.left,!needDelete))
+            {
+                //左子节点需要删除
+                root.left = null;
+            }
+
+            if (PostOrder(root.right,!needDelete))
+            {
+                //右子节点需要删除
+                root.right = null;
+            }
+
+            if (!needDelete && !parentExist)
+            {
+                //自身不需要删除 并且父节点不存在 就加入ans
+                ans.Add(root);
+            }
+
+            return needDelete;
         }
     }
 }
