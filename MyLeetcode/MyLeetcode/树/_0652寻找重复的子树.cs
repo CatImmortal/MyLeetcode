b09b34fa201fbc34bc261bcbc5e6ed731d649b7c
CatImmortal <1304085906@qq.com>
2020-08-03 19:46:24 +0800

    提交树相关题目

--- MyLeetcode/MyLeetcode/树/_0652寻找重复的子树.cs
+++ MyLeetcode/MyLeetcode/树/_0652寻找重复的子树.cs
@@ -10,10 +10,63 @@ namespace MyLeetcode.树
 
     class _0652寻找重复的子树
     {
+        private int IdCounter = 1;
+        private Dictionary<string, int> treesDict = new Dictionary<string, int>();
+        private Dictionary<int, int> countDict = new Dictionary<int, int>();
+        private List<TreeNode> ans = new List<TreeNode>();
 
         public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
         {
-           
+            PostOrder(root);
+
+            return ans;
+        }
+
+        private int PostOrder(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            string leftSerial = PostOrder(root.left).ToString();
+            string rightSerial = PostOrder(root.right).ToString();
+
+            string rootSerial = $"{root.val.ToString()},{leftSerial},{rightSerial}";
+
+            //获取子树序列化对应的uid
+            if (!treesDict.TryGetValue(rootSerial, out int uid))
+            {
+                uid = IdCounter;
+                treesDict.Add(rootSerial, uid);
+                ++IdCounter;
+            }
+
+            //统计此uid出现的次数
+            if (!countDict.TryGetValue(uid,out int count))
+            {
+                count = 1;
+                countDict.Add(uid, 1);
+            }
+            else
+            {
+                countDict[uid] += 1;
+            }
+
+            //第二次重复出现 加入解答列表
+            if (countDict[uid] == 2)
+            {
+                ans.Add(root);
+            }
+
+            return uid;
+
+          
+
+
+
+
+
         }
 
 
---
+++ MyLeetcode/MyLeetcode/树/_0865具有所有最深结点的最小子树.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeetcode.树
+{
+    //https://leetcode-cn.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+
+    class _0865具有所有最深结点的最小子树
+    {
+        public TreeNode SubtreeWithAllDeepest(TreeNode root)
+        {
+            List<TreeNode> result = LevelOrder(root);
+
+            TreeNode node1 = result[0];
+
+            for (int i = 1; i < result.Count; i++)
+            {
+                TreeNode node2 = result[i];
+
+                //不断调用这个方法 获取node1 和 node2 的最近公共祖先
+                node1 = LowestCommonAncestor(root, node1, node2);
+            }
+
+            return node1;
+        }
+
+        /// <summary>
+        /// 层序遍历获取最底下一层的节点
+        /// </summary>
+        private List<TreeNode> LevelOrder(TreeNode root)
+        {
+            List<TreeNode> result = new List<TreeNode>();
+            if (root == null)
+            {
+                return result;
+            }
+            Queue<TreeNode> queue1 = new Queue<TreeNode>();
+            Queue<TreeNode> queue2 = new Queue<TreeNode>();
+
+            queue1.Enqueue(root);
+
+            while (queue1.Count > 0)
+            {
+
+                //把queue1里的所有node都出队，并把它们的子节点都暂时放进queue2里
+                while (queue1.Count > 0)
+                {
+                    TreeNode node = queue1.Dequeue();
+
+                    result.Add(node);
+
+                    if (node.left != null)
+                    {
+                        queue2.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue2.Enqueue(node.right);
+                    }
+                }
+
+
+                //把queue2里的所有node都放进queue1中
+                while (queue2.Count > 0)
+                {
+                    queue1.Enqueue(queue2.Dequeue());
+                    result.Clear();
+                }
+            }
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// 获取p和q在root下的最近公共祖先
+        /// </summary>
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || root == p || root == q)
+            {
+                return root;
+            }
+
+            // LowestCommonAncestor的返回值如果不为null（p和q都不存在root下）
+            //那么返回值是p和q其中至少一个的公共祖先（或者是其本身）
+
+            TreeNode leftNode = LowestCommonAncestor(root.left, p, q);
+            TreeNode rightNode = LowestCommonAncestor(root.right, p, q);
+
+            if (leftNode != null && rightNode != null)
+            {
+                //p和q分别在左子树和右子树中 root是它俩的公共祖先
+                return root;
+            }
+
+            if (leftNode != null)
+            {
+                //p和q都在左子树 leftNode是它俩的公共祖先
+                return leftNode;
+            }
+
+            if (rightNode != null)
+            {
+                //p和q都在右子树 rightNode是它俩的公共祖先
+                return rightNode;
+            }
+
+            return null;
+        }
+    }
+}
