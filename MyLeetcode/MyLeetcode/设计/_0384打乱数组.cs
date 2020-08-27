6fabe4daca08ca36fb659a9b956573fe84242589
CatImmortal <1304085906@qq.com>
2020-08-27 19:26:56 +0800

    提交设计问题

---
+++ MyLeetcode/MyLeetcode/设计/_0384打乱数组.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeetcode.设计
+{
+    //https://leetcode-cn.com/problems/shuffle-an-array/
+
+    class _0384打乱数组
+    {
+        public class Solution
+        {
+
+            public Solution(int[] nums)
+            {
+
+            }
+
+            /** Resets the array to its original configuration and return it. */
+            public int[] Reset()
+            {
+
+            }
+
+            /** Returns a random shuffling of the array. */
+            public int[] Shuffle()
+            {
+
+            }
+        }
+    }
+}
